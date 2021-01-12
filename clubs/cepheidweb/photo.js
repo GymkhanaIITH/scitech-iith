3f4b4c5bfec83e96792038e1d3ff3c05cf58e845
Rachit Keerti Das <rachitkdas@gmail.com>
2021-01-12 17:07:32 +0530

    Add New Cepheid Website.Rename Old Website Folder.

--- clubs/cepheidweb/photo.js
+++
@@ -1,9 +0,0 @@
-function getRandomSize(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-for (var i = 0; i < 25; i++) {
-  var width = getRandomSize(20, 40);
-  var height =  getRandomSize(20, 40);
-  $('#photos').append('<img src="//www.lorempixel.com/'+width+'/'+height+'/cats" alt="pretty kitty">');
-}
